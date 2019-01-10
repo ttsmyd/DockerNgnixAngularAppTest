6a96dad1fe4df464e721e9ce433db855f145ce80
ttsmyd <dmitry4884@yandex.ru>
2019-01-10 16:24:47 +0300

    HelloWorldProject

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { StrategiesComponent } from './strategies/strategies.component';
+import { FriendsComponent } from './friends/friends.component';
+import { LogoutComponent } from './logout/logout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'strategies', component: StrategiesComponent},
+  {path: 'friends', component: FriendsComponent},
+  {path: 'logout', component: LogoutComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { StrategiesComponent } from './strategies/strategies.component';
+import { FriendsComponent } from './friends/friends.component';
+import { LogoutComponent } from './logout/logout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FooterComponent,
+    HomeComponent,
+    StrategiesComponent,
+    FriendsComponent,
+    LogoutComponent
   ],
   imports: [
     BrowserModule,
